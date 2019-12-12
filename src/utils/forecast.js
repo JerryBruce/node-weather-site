94b281dbc144076368a6001da5ab8111468e6961
Jerry Bruce <bruc3y33@gmail.com>
2019-12-11 23:24:16 -0600

    Added the high/low for the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/714984dcbc690142f3c437558bb61674/${encodeURIComponent(latitude)}, ${encodeURIComponent(longitude)}`
     request({ url, json: true }, (error, {body}) => {
@@ -10,7 +12,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location. Try again', undefined)
         } else {
            callback(undefined,
-               `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees. There is a ${body.currently.precipProbability}% chance of rain.`)
+               `${body.daily.data[0].summary} The high for today is ${body.daily.data[0].temperatureHigh} and the low is ${body.daily.data[0].temperatureLow}.
+                It is currently ${body.currently.temperature} degrees. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 }
